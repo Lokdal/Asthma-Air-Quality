e44329ff42832f75e8df730f7a077aa5dfd22032
Vincent LeBlanc <leblvin@gmail.com>
2019-02-12 17:44:26 -0500

    Cleanup
    Cleaned up the code, renamed figures, etc.

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 # In[]:
-# ======
-# Import libraries and define functions
+# Import libraries and set options
 
 import pandas
 import seaborn
@@ -9,14 +8,11 @@ import json
 import folium
 import branca
 
-# In[]:
-# =====
-# Set options
-
 pandas.set_option("display.max_columns", 7)
 pandas.set_option("display.max_colwidth", 30)
+# ======
+
 # In[]:
-# =====
 # Define functions
 
 
@@ -56,11 +52,13 @@ def fGetCountyFIPSFromCountyState(iDFCountyState):
     return tFIPS
 
 
+# ======
+
 # In[]:
-# =====
 # Import data
 
 # https://chronicdata.cdc.gov/500-Cities/500-Cities-Local-Data-for-Better-Health-2018-relea/6vp6-wxuq
+# This is a 220MB csv file.
 # Click on Export > CSV
 iDBHealth = pandas.read_csv(
     "data\\500_Cities__Local_Data_for_Better_Health__2017_release.csv",
@@ -101,9 +99,9 @@ iDBAir["County"] = iDBAir["County"].apply(str.rstrip)
 # Upload .zip file to mapshaper.org and export as GeoJSON
 with open("data\\cb_2015_us_county_5m.json", "r") as geo:
     iGeoData = json.load(geo)
+# =====
 
 # In[]:
-# =====
 # Select and prepare the asthma and smoking data
 
 wDBHealth = (iDBHealth.loc[
@@ -144,9 +142,9 @@ wDBHealth.loc[wDBHealth.index[110], 'County'] = "Ventura"
 wDBHealth.loc[wDBHealth.index[220], 'County'] = "Ada"
 wDBHealth.loc[wDBHealth.index[307], 'County'] = "Jackson"
 wDBHealth.loc[wDBHealth.index[461], 'County'] = "Salt Lake"
+# =====
 
 # In[]:
-# =====
 # Select and prepare the air quality database
 wDBAir = iDBAir.copy()
 wDBAir["County"] = (
@@ -161,15 +159,15 @@ wDBAir.loc[wDBAir.index[549], 'County'] = "Carson City (city)"
 wDBAir.loc[wDBAir.index[584], 'County'] = "Doña Ana"
 wDBAir.loc[wDBAir.index[941], 'County'] = "Charles City"
 
+# Get the county FIPS of each county (some were found manually)
 wDBCities = iDBCities.drop_duplicates(["State", "County"])
-
 wDBAir = pandas.concat([wDBAir,
                         fGetCountyFIPSFromCountyState
                         (wDBAir[["State", "County"]])], axis=1)
 wDBAir = wDBAir.drop([143, 514, 808, 809, 933, 934], axis=0)
+# =====
 
 # In[]:
-# =====
 # Merge the datasets and cleanup
 
 wDBAsthma = wDBHealth.merge(wDBAir, on="County FIPS")
@@ -196,13 +194,6 @@ wDBAsthma = wDBAsthma[['State', 'County', "County FIPS", 'City', "Coords",
                        "Asthma Prevalence", "Smoking Prevalence", "Median AQI",
                        "Proportion of days with bad air quality"]]
 
-fColor = (
-    branca
-    .colormap
-    .LinearColormap(["green", "yellow", "red"],
-                    vmin=min(wDBAsthma["Asthma Prevalence"]),
-                    vmax=max(wDBAsthma["Asthma Prevalence"])))
-wDBAsthma["Color"] = wDBAsthma["Asthma Prevalence"].apply(fColor)
 
 # In[]:
 oFig, oAx = matplotlib.pyplot.subplots(figsize=(11.7, 8.27))
@@ -214,17 +205,19 @@ oAsthmaSmokeAQI.set_ylabel("Age Adjusted Smoking Prevalence")
 oAsthmaSmokeAQI.get_legend().set_title("Age Ajusted ")
 oAsthmaSmokeAQI.set(title="Relationship between AQI, smoking and asthma in "
                           + "474 big cities of the USA (2015)")
-oAsthmaSmokeAQI.get_figure().savefig("Asthma-Smoking 2015.png")
+oAsthmaSmokeAQI.get_figure().savefig("Asthma Smoking and AQI.jpg")
+# =====
 
 # In[]:
+# Create a choropleth map of air quality
+oMap = folium.Map((39.8283, -98.5795), zoom_start=5, tiles="Stamen Toner")
+
+# Only use the counties for which air quality data is available
 wGeoData = dict({"type": "FeatureGroup", "features": []})
 tListFIPS = set(wDBAir["County FIPS"].tolist())
 for f in iGeoData["features"]:
     if f["properties"]["GEOID"] in tListFIPS:
         wGeoData["features"].append(f)
-
-# In[]:
-oMap = folium.Map((39.8283, -98.5795), zoom_start=5, tiles="Stamen Toner")
 wChoroLayer = folium.Choropleth(wGeoData,
                                 data=wDBAir,
                                 name="Air Quality Index",
@@ -233,7 +226,15 @@ wChoroLayer = folium.Choropleth(wGeoData,
                                 fill_color="YlOrRd",
                                 fill_opacity=0.9)
 
+# Place the cities and color their marker according to the asthma prevalence
 wCityLayer = folium.FeatureGroup(name="Cities")
+fColor = (
+    branca
+    .colormap
+    .LinearColormap(["green", "yellow", "red"],
+                    vmin=min(wDBAsthma["Asthma Prevalence"]),
+                    vmax=max(wDBAsthma["Asthma Prevalence"])))
+wDBAsthma["Color"] = wDBAsthma["Asthma Prevalence"].apply(fColor)
 
 for name, col, coord in zip(wDBAsthma["City"], wDBAsthma["Color"],
                             wDBAsthma["Coords"]):
@@ -241,7 +242,8 @@ for name, col, coord in zip(wDBAsthma["City"], wDBAsthma["Color"],
         folium.CircleMarker(location=coord, tooltip=name, radius=3,
                             fill_color=col, fill_opacity=1, color=None))
 
+
 oMap.add_child(wChoroLayer)
 oMap.add_child(wCityLayer)
 oMap.add_child(folium.LayerControl())
-oMap.save("AQI.html")
+# oMap.save("Asthma and Air Quality map.html")
